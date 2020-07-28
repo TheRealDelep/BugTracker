3d1053a0e0f3ae26287baf903154a58f3f1723f6
Sylvain Delépée <sylvain.delepee@gmail.com>
2020-07-28 10:54:17 +0200

    Creating LogRepo Class/ Not Implemented/ Writing GetById test

---
+++ BugTracker.DAL.Tests/LogRepositoryTests.cs
@@ -0,0 +1,62 @@
+﻿using BugTracker.Cross_Cutting.Models;
+using BugTracker.DAL.Repositories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BugTracker.DAL.Tests
+{
+    public class LogRepositoryTests
+    {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BugTracker.ServiceTests;Integrated Security=True;";
+        private SqlConnection connection;
+        private SqlTransaction transaction;
+        private LogRepository logRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            transaction = connection.BeginTransaction("transaction");
+            logRepo = new LogRepository(connection, transaction);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            transaction.Commit();
+            connection.Close();
+        }
+
+        #region GetById
+
+        [Test]
+        public void Should_Return_TestLog_At_Id_1()
+        {
+            var testLog = new Log()
+            {
+                Id = 1,
+                Author = new User()
+                {
+                    Id = 1,
+                    IdentityId = 1
+                },
+                Title = "Test",
+                Body = "Hi, I am testing my repo",
+                Date = new DateTime(),
+            };
+
+            var dbLog = logRepo.GetById(testLog.Id);
+
+            Assert.AreEqual(testLog.Author.Id, dbLog.Author.Id);
+            Assert.AreEqual(testLog.Title, dbLog.Title);
+            Assert.AreEqual(testLog.Body, dbLog.Body);
+            Assert.AreEqual(testLog.Date, dbLog.Date);
+        }
+
+        #endregion GetById
+    }
+}
---
+++ BugTracker.DAL/Repositories/LogRepository.cs
@@ -0,0 +1,42 @@
+﻿using Bugtracker.Services.BugTracker.DAL.Repositories;
+using BugTracker.Cross_Cutting.Interfaces;
+using BugTracker.Cross_Cutting.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BugTracker.DAL.Repositories
+{
+    public class LogRepository : ADORepository<Log>
+    {
+        public LogRepository(SqlConnection connection, SqlTransaction transaction) : base(connection, transaction)
+        {
+        }
+
+        public override bool Delete(Log entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Log> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Log GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Log Insert(Log entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Log Update(Log entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
