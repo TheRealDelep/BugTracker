939f74c3980db5e309d9d0ab789d5e4d20da5679
Sylvain Delépée <sylvain.delepee@gmail.com>
2020-07-28 10:40:11 +0200

    Creating Log and Issue Models

---
+++ BugTracker.Cross_Cutting/Models/Issue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Cross_Cutting.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public Log Title { get; set; }
+        public List<Log> Comments { get; set; }
+        public DateTime LatestUpdate { get; set; }
+    }
+}
---
+++ BugTracker.Cross_Cutting/Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Cross_Cutting.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public User Author { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
